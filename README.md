aad8678b029b253f204cbf5ceb0615b686becb99
Ricky Cordero <8922867+RickyCordero@users.noreply.github.com>
2020-07-10 20:18:09 -0400

    Added dynamic rendering support

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# june
+# june-node
 
 A simple file scraper with Node.js
 
@@ -22,11 +22,11 @@ npm i
 
 Set the webpage url in ```main.js``` to the desired page containing files for download:
 
-```const WEBPAGE_URL = 'https://...'```
+```const WEBPAGE = 'https://...'```
 
 Then modify the file types array to contain only the file types you want to download:
 
-```const FILE_TYPES = ["mp4", "mov", ... ]```
+```const FILE_TYPES = ["mp4", "gif", ... ]```
 
 To start the script, run the following command:
 
@@ -34,24 +34,49 @@ To start the script, run the following command:
 npm start
 ```
 
-All files found in an anchor tag on the given webpage will be downloaded in parallel and written to the current folder with their original file names.
+If the webpage to be scraped requires login credentials, specify the login credentials in ```main.js```:
+
+```
+// Set the username and password to automate login if needed
+const USERNAME = "";
+const PASSWORD = "";
+```
+and specify the css selectors for the username input box, the password input box, and the login/submit button:
+
+```
+// Set the html elements to automate login if needed
+const USERNAME_ELEMENT = ""; // css selector for input box username field on webpage
+const PASSWORD_ELEMENT = ""; // css selector for input box password field on webpage
+const LOGIN_ELEMENT = ""; // css selector for login button on webpage
+```
+
+All files found in an anchor, image, or link tag on the given webpage will be downloaded in parallel and written to the current folder with their original file names. If other tags are needed or different attributes should be scraped, add the desired tag to ```TAG_TYPES``` and the desired tag/attribute pair to ```TAG_ATTR_MAP```:
+
+```
+// Tag and attribute types
+const TAG_TYPES = ["a", "img", "link"]; // html tags to scrape
+const TAG_ATTR_MAP = { // attributes to scrape for each tag
+    "a": "href",
+    "img": "src",
+    "link": "href"
+};
+```
 
 ## Debugging
 
 If you encounter an error during downloading, check your connection first, but lower the number of concurrent requests in case throttling is suspected:
 
 ```
-const ASYNC_LIMIT = 1;
+// Number of files to download in parallel
+const ASYNC_LIMIT = 2; // decrease to 1 if throttling suspected, increase if download is too slow
 ```
 
 If the downloads are blocking for too long, increase the limit.
 
-## TODO:
-
-- Authentication handling
-
 ## Built With
 
+* [nightmare](https://github.com/segmentio/nightmare) - Browser automation library
 * [jsdom](https://github.com/jsdom/jsdom) - JavaScript based headless browser
 * [async](https://caolan.github.io/async/v3/) - Asynchronous processing library
+* [request](https://github.com/request/request) - HTTP library
 
--- main.js
+++ main.js
@@ -1,73 +1,176 @@
 const fs = require("fs");
 const async = require("async");
-const got = require('got');
 const request = require('request');
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
+const Nightmare = require("nightmare");
+const { JSDOM } = require("jsdom");
+const nightmare = Nightmare({ show: true }); // monitor what the browser is doing on execution
 
-const WEBPAGE_URL = 'https://www.learningcontainer.com/mp4-sample-video-files-download/'; // webpage to scrape
-const FILE_TYPES = ["ogv", "mp4", "3g2"]; // file types to download
-const ASYNC_LIMIT = 2; // decrease to 1 if getting download error, increase if download too slow
+// Set the desired webpage and file types to scrape
+const WEBPAGE = 'https://www.learningcontainer.com/mp4-sample-video-files-download/'; // webpage to scrape
+const FILE_TYPES = ["mp4", "gif", "png", "jpg", "webp"]; // file types to download
+
+// Set the username and password to automate login if needed
+const USERNAME = "";
+const PASSWORD = "";
+
+// Set the html elements to automate login if needed
+const USERNAME_ELEMENT = ""; // css selector for input box username field on webpage
+const PASSWORD_ELEMENT = ""; // css selector for input box password field on webpage
+const LOGIN_ELEMENT = ""; // css selector for login button on webpage
+const NEW_PAGE_TIMEOUT = 5000; // ms to wait for a page to render
+
+// Tag and attribute types
+const TAG_TYPES = ["a", "img", "link"]; // html tags to scrape
+const TAG_ATTR_MAP = { // attributes to scrape for each tag
+    "a": "href",
+    "img": "src",
+    "link": "href"
+};
+
+// Number of files to download in parallel
+const ASYNC_LIMIT = 2; // decrease to 1 if throttling suspected, increase if download is too slow
+
+// A simple class for link processing
+class Link {
+    constructor(tag, attr) {
+        this.tag = tag;
+        this.attr = attr;
+    }
+}
 
 /**
  * Determines if the html element contains a desired file type
- * @param {Object} link - The html element
+ * @param {Link} link - The html element
  */
 const isFileType = (link) => {
-    // Return false if there is no href attribute in the element
-    if (typeof link.href === 'undefined') { return false }
-    // Check if any desired file type is found in the link
-    return FILE_TYPES.some(fileType => link.href.includes(`.${fileType.toLowerCase()}`));
+    // return false if there is no attribute in the Link
+    if (typeof link.attr === 'undefined') { return false }
+    // check if any desired file type is found in the Link's attribute
+    return FILE_TYPES.some(fileType => link.attr.includes(`.${fileType.toLowerCase()}`));
 };
 
-(async () => {
-    const response = await got(WEBPAGE_URL);
-    const dom = new JSDOM(response.body);
+// Simulate browser interaction
+if (USERNAME && PASSWORD && USERNAME_ELEMENT && PASSWORD_ELEMENT && LOGIN_ELEMENT) {
+    // if login needed
+    nightmare
+        .goto(WEBPAGE) // go to page
+        .wait(USERNAME_ELEMENT) // wait for page to render
+        .type(USERNAME_ELEMENT, USERNAME) // type username
+        .type(PASSWORD_ELEMENT, PASSWORD) // type password
+        .click(LOGIN_ELEMENT) // login
+        .wait(NEW_PAGE_TIMEOUT) // wait for new page
+        .evaluate(() => document.querySelector('body').innerHTML) // fetch the rendered html
+        .end() // close the browser
+        .then(response => processHtml(response)) // process the html
+        .catch(err => {
+            console.log(err);
+        });
+} else {
+    // no login needed
+    nightmare
+        .goto(WEBPAGE) // go to page
+        .wait(NEW_PAGE_TIMEOUT) // wait for page
+        .evaluate(() => document.querySelector('body').innerHTML) // fetch the rendered html
+        .end() // close the browser
+        .then(response => processHtml(response)) // process the html
+        .catch(err => {
+            console.log(err);
+        });
+}
 
-    // Create an array of all anchor (link) elements
-    const links = [...dom.window.document.querySelectorAll('a')];
-    const hrefs = [...new Set(
+/**
+ * Downloads a URL to a local file.
+ * @param {URL} url - The url of the file to be downloaded
+ * @param {String} outputFileName - The name of the output file
+ */
+const downloadFile = (url, outputFileName) => new Promise((resolve, reject) => {
+    // options for http request
+    const options = {
+        uri: url.toString(),
+        headers: {
+            'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.129 Safari/537.36",
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        gzip: true
+    };
+    // initiate http request
+    request(options)
+        .pipe(fs.createWriteStream(outputFileName))
+        .on('finish', () => {
+            console.log(`  ✅ Successfully downloaded ${url.toString()} to ${outputFileName}` + '\n');
+            resolve();
+        })
+        .on('error', (err) => {
+            console.log(`  ❌ There was an error downloading ${url.toString()} to ${outputFileName}` + '\n');
+            reject(err);
+        });
+});
+
+/**
+ * Process a URL before downloading to a local file.
+ * @param {URL} url - The url to be processed
+ * @param {Function} cb - The next function
+ */
+const processThenDownload = (url, cb) => {
+    // extract name of resource
+    const urlPathName = url.pathname.split("/").pop();
+    // initiate file stream
+    downloadFile(url, urlPathName)
+        .then(() => cb())
+        .catch(err => {
+            cb({ file: url.toString(), error: err });
+        });
+};
+
+/**
+ * Extract and download all desired files from an html webpage
+ * @param {String} html - An html webpage string
+ */
+const processHtml = html => {
+    // create the dom tree
+    const dom = new JSDOM(html);
+
+    // map all tree nodes to Links
+    const links = TAG_TYPES.reduce((acc, tag) => {
+        return acc.concat(
+            [...dom.window.document.querySelectorAll(tag)]
+                .map(node => new Link(tag, node[TAG_ATTR_MAP[tag]])));
+    }, []);
+
+    // extract the unique attribute strings from all Links
+    const attributes = [...new Set(
         links
             .filter(isFileType)
-            .map(link => link.href)
+            .map(link => link.attr)
     )];
-    // Download each file
-    console.log(`>> Downloading ${hrefs.length} file(s)...` + '\n');
-    hrefs.forEach(href => console.log(href));
-    console.log("\n");
-    async.eachOfLimit(hrefs, ASYNC_LIMIT, (href, _idx, cb) => {
-        const uri_1 = href;
-        const baseUrl = WEBPAGE_URL.substring(0, WEBPAGE_URL.lastIndexOf("/"));
-        const urlFileName = href.split("/").pop();
-        const outputFileName = urlFileName;
-        const downloadFile = (uri) => new Promise((resolve, reject) => {
-            request({
-                uri: uri,
-                headers: {},
-                gzip: true
-            })
-                .pipe(fs.createWriteStream(outputFileName))
-                .on('finish', () => {
-                    console.log(`  ✅ Successfully downloaded ${uri} to ${outputFileName}` + '\n');
-                    resolve();
-                })
-                .on('error', (err) => {
-                    console.log(`  ❌ There was an error downloading ${uri} to ${outputFileName}` + '\n');
-                    reject(err);
-                });
-        });
-        // Initiate file stream
-        downloadFile(uri_1)
-            .then(() => cb())
-            .catch(err_1 => {
-                const uri_2 = `${baseUrl}/${urlFileName}`;
-                downloadFile(uri_2)
-                    .then(() => cb())
-                    .catch(err_2 => {
-                        cb({ file: uri, error: err_1 });
-                        cb({ file: uri_2, error: err_2 })
-                    });
+
+    // download each file in parallel
+    console.log(`>> Downloading ${attributes.length} file(s)...` + '\n');
+    async.eachOfLimit(attributes, ASYNC_LIMIT, (attr, _idx, cb) => {
+        try {
+            // case 1: attribute is a full url
+            // e.g. http://website.com/image.png
+            const url = new URL(attr);
+            processThenDownload(url, (err) => {
+                if (err) {
+                    cb({ file: url.toString(), error: err });
+                } else {
+                    cb();
+                }
             });
+        } catch (error) {
+            // case 2: attribute is a relative path
+            // e.g. ./image.png
+            const url = new URL(WEBPAGE);
+            url.pathname = attr;
+            processThenDownload(url, (err) => {
+                if (err) {
+                    cb({ file: url.toString(), error: [error, err] });
+                } else {
+                    cb();
+                }
+            });
+        }
     }, (err) => {
         if (err) {
             console.log(">> A file failed to download:");
@@ -76,4 +179,4 @@ const isFileType = (link) => {
             console.log("❤  All files downloaded successfully.");
         }
     });
-})();
+};
